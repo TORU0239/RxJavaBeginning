dec698745608e8fb213c8ff0ce322afc8ddc9fff
Toru Choi <toru.choi@11street.my>
2019-01-24 18:35:40 +0800

    trying to make a new list

--- app/src/main/java/sg/toru/rxjavatest/ui/RxRecycleActivity.java
+++ app/src/main/java/sg/toru/rxjavatest/ui/RxRecycleActivity.java
@@ -2,13 +2,23 @@ package sg.toru.rxjavatest.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
 
 import sg.toru.rxjavatest.R;
+import sg.toru.rxjavatest.data.DummyModel;
+import sg.toru.rxjavatest.ui.adapter.RxRecyclerAdapter;
 
 public class RxRecycleActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private RxRecyclerAdapter adapter;
+    private ArrayList<DummyModel> dummyModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +27,42 @@ public class RxRecycleActivity extends AppCompatActivity {
 
         init();
     }
-
+    private int index = 4;
     private void init(){
+        dummyModels = new ArrayList<>();
         recyclerView = findViewById(R.id.rcv_rx);
+        adapter = new RxRecyclerAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(adapter);
+
+        for(int i = 0; i < 5; i++){
+            DummyModel model = new DummyModel();
+            model.setType("TYPE");
+            model.setIndex(String.valueOf(i));
+            dummyModels.add(model);
+        }
+        adapter.submitList(dummyModels);
+
+
+        findViewById(R.id.btn_add).setOnClickListener(v -> {
+            ArrayList<DummyModel> models = new ArrayList<>();
+            for(int i = 6; i < 10; i++){
+                DummyModel model = new DummyModel();
+                model.setType("ADDED");
+                model.setIndex(String.valueOf(++index));
+                models.add(model);
+            }
+//            models.addAll(0, dummyModels);
+//
+//            DummyModel model = new DummyModel();
+//            model.setType("ADDED");
+//            model.setIndex(String.valueOf(++index));
+//            models.add(model);
+//
+//            dummyModels.add(model);
+            adapter.submitList(models);
+//            recyclerView.scrollToPosition(adapter.getItemCount()-1);
+        });
     }
 }
--- app/src/main/java/sg/toru/rxjavatest/ui/adapter/RxRecyclerAdapter.java
+++ app/src/main/java/sg/toru/rxjavatest/ui/adapter/RxRecyclerAdapter.java
@@ -1,25 +1,21 @@
 package sg.toru.rxjavatest.ui.adapter;
 
 import android.support.annotation.NonNull;
-import android.support.v7.recyclerview.extensions.AsyncDifferConfig;
 import android.support.v7.recyclerview.extensions.ListAdapter;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import sg.toru.rxjavatest.R;
 import sg.toru.rxjavatest.data.DummyModel;
 
 public class RxRecyclerAdapter extends ListAdapter<DummyModel, RxRecyclerAdapter.RxRecyclerVH> {
 
-    protected RxRecyclerAdapter(@NonNull DiffUtil.ItemCallback<DummyModel> diffCallback) {
-        super(diffCallback);
-    }
-
-    protected RxRecyclerAdapter(@NonNull AsyncDifferConfig<DummyModel> config) {
-        super(config);
+    public RxRecyclerAdapter() {
+        super(new ListItemCallback());
     }
 
     @NonNull
@@ -27,21 +23,38 @@ public class RxRecyclerAdapter extends ListAdapter<DummyModel, RxRecyclerAdapter
     public RxRecyclerVH onCreateViewHolder(@NonNull ViewGroup container, int viewType) {
         View itemView = LayoutInflater.from(container.getContext()).inflate(R.layout.adapter_simple, container, false);
         return new RxRecyclerVH(itemView);
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull RxRecyclerVH rxRecyclerVH, int position) {
-        rxRecyclerVH.bindItem();
+        rxRecyclerVH.bindItem(getItem(position));
     }
 
     static class RxRecyclerVH extends RecyclerView.ViewHolder{
+        private TextView index;
+        private TextView type;
         public RxRecyclerVH(@NonNull View itemView) {
             super(itemView);
+            type = itemView.findViewById(R.id.txt_sort);
+            index = itemView.findViewById(R.id.txt_item);
         }
 
-        protected void bindItem(){
-
+        protected void bindItem(DummyModel model){
+            type.setText(model.getType());
+            index.setText(model.getIndex());
         }
+    }
+}
+
+class ListItemCallback extends DiffUtil.ItemCallback<DummyModel>{
+
+    @Override
+    public boolean areItemsTheSame(@NonNull DummyModel oldItem, @NonNull DummyModel newItem) {
+        return (oldItem == newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull DummyModel oldItem, @NonNull DummyModel newItem) {
+        return (oldItem.getIndex().equals(newItem.getIndex()));
     }
 }
